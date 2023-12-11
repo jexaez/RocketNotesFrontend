d6e9c2b30a92b5adee2b882464c7b3af8e4f85fb
jani <exaezsa@gmail.com>
2023-12-11 18:13:48 -0400

    Criando componente botão em react

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+export function Button({ title, loading = false }){
+    return(
+        <Container 
+        type="button"
+        disabled={loading}>
+            { loading ? 'Carregando...' : title }
+        </Container>
+    )
+}
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,13 @@
+import {Container} from "./styles"
+import { Button } from "../../components/Button"
+
+export function Details(){
+  return(
+    <Container>
+      <h1>Hola Mundo!</h1>
+      <span>Jani Ochoa</span>
+      <Button title = 'Login' />
+    </Container>
+    
+  )
+}
